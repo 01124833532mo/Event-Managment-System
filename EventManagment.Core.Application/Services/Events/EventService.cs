334990f73db7e4b0ac268ee5eb2915ebfbc91ae2
mohamed_hamdi_mahmoud <mohammedhamdi726@gmail.com>
2025-03-08 22:20:23 +0200

    Add Notification For Attendess Whene The Event Creation

--- EventManagment.Core.Application/Services/Events/EventService.cs
+++ EventManagment.Core.Application/Services/Events/EventService.cs
@@ -1,17 +1,20 @@
 ﻿using AutoMapper;
 using EventManagment.Core.Application.Abstraction.Bases;
+using EventManagment.Core.Application.Abstraction.Services.Emails;
 using EventManagment.Core.Application.Abstraction.Services.Events;
 using EventManagment.Core.Domain.Contracts.Persestence;
 using EventManagment.Core.Domain.Entities._Identity;
 using EventManagment.Core.Domain.Entities.Categories;
 using EventManagment.Core.Domain.Entities.Events;
 using EventManagment.Shared.Errors.Models;
+using EventManagment.Shared.Models._Common.Emails;
 using EventManagment.Shared.Models.Events;
+using EventManagment.Shared.Models.Roles;
 using Microsoft.AspNetCore.Identity;
 
 namespace EventManagment.Core.Application.Services.Events
 {
-    public class EventService(IUnitOfWork _unitOfWork, IMapper _mapper, UserManager<ApplicationUser> userManager) : ResponseHandler, IEventServices
+    public class EventService(IUnitOfWork _unitOfWork, IMapper _mapper, UserManager<ApplicationUser> userManager, IEmailService emailService) : ResponseHandler, IEventServices
     {
         public async Task<Response<EventToreturn>> CreateEvent(EventDto eventDto)
         {
@@ -36,6 +39,21 @@ namespace EventManagment.Core.Application.Services.Events
             }
 
             mappedresult.OrganizerName = FullNameUser.FullName;
+
+
+            var allattendees = await userManager.GetUsersInRoleAsync(Roles.Attendee);
+            foreach (var attendee in allattendees)
+            {
+                var emailSubject = "New Event Created";
+                var emailMessage = $"Dear {attendee.FullName},\n\nA new event '{mappedresult.CategoryName}' has been created Do You Want To Register For This Event?.\n\nEvent Date: {mappedresult.Data}\n\nThank you!";
+                var email = new Email()
+                {
+                    Subject = emailSubject,
+                    Body = emailMessage,
+                    To = attendee.Email!
+                };
+                await emailService.SendEmail(email);
+            }
 
             return Created(mappedresult);
 
